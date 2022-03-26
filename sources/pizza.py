324e52431f379ccff127f12d93ebd385b313023e
Stéphane Grosheitsch <willerstaf@gmail.com>
2022-03-26 08:37:50 +0100

    new pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,10 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'new pizz': {
+        'ingredients': {'mozzarella': 3, 'onion': 3},
+        'price': 11,
+
     }
 }
